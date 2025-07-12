a56ea6ee21c2a50e323ba2deeec5d8002d5a154e
Daniel Wright <daniel.wright2@dcmail.ca>
2025-07-11 23:09:49 -0400

    Commit For class

--- Week9/day2/views/about.php
+++ Week9/day2/views/about.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <title>About</title>
-    <link rel="stylesheet" href="../Styles/styles.css">
+    <link rel="stylesheet" href="../styles/styles.css">
 </head>
 <body>
 
