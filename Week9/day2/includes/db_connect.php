4fa33e4c5783eb3345ae47734e51410a75628a1f
Daniel Wright <daniel.wright2@dcmail.ca>
2025-07-11 23:20:18 -0400

    Commit For class

--- Week9/day2/includes/db_connect.php
+++ Week9/day2/includes/db_connect.php
@@ -4,7 +4,7 @@ function get_db_connection() {
 
     $conn = pg_connect("host=host.docker.internal port 5432 dbname=portfolio user=admin password=password");
     if(!$conn) {
-        die("Database connection failed: " . pg_last_error());
+        die("Database connection failed: " . pg_last_error() . pg_last_error());
     }
     return $conn;
 }
